7b74a5688bcbbbf45ea9ef24af20e89b6f21171a
Pedro Stanzani <pedrohstanzani@gmail.com>
2024-06-01 10:21:39 -0300

    create next.js project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="max-w-2xl mx-auto mt-8">
+        <h1 className="text-balance scroll-m-20 text-2xl font-semibold tracking-tight">
+          Integrating Next.js with a backend server using an auth provider
+        </h1>
+      </div>
+    </main>
+  );
+}
